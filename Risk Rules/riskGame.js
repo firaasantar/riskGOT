4900612aac256c2795c6771b026face315916a1e
firaasantar <85679952+firaasantar@users.noreply.github.com>
2023-07-31 11:30:48 +0300

    added every thing

---
+++ Risk Rules/riskGame.js
@@ -0,0 +1,6 @@
+function showCountryInfo(countryName) {
+    // Replace the following alert with code to display the information you want
+
+    let t = document.getElementById(countryName).innerText;
+    alert("Information about " + t);
+}
